01cb848a779ed22019ca9e16b37321968f587dc8
Praveen-Sharma00 <52413182+Praveen-Sharma00@users.noreply.github.com>
2020-09-06 18:01:12 +0530

    Basic layout done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,33 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {PostCreateComponent} from './posts/post-create/post-create.component';
 import {FormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {HeaderComponent} from './header/header.component';
+import {PostListComponent} from './posts/post-list/post-list.component';
+import {MatAccordion, MatExpansionModule} from '@angular/material/expansion';
+
 
 @NgModule({
   declarations: [
-    AppComponent, PostCreateComponent
+    AppComponent, PostCreateComponent, HeaderComponent, PostListComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,29 @@
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {Post} from '../post.model';
+import {PostService} from '../post.service';
+import {Subscription} from 'rxjs';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit, OnDestroy {
+  posts: Post[] = [];
+  private postSub: Subscription;
+
+  constructor(public postService: PostService) {
+  }
+
+  ngOnInit() {
+    this.posts = this.postService.getPosts();
+    this.postSub = this.postService.getPostUpdatedListener()
+      .subscribe((posts) => {
+        this.posts = posts;
+      });
+  }
+
+  ngOnDestroy() {
+    this.postSub.unsubscribe();
+  }
+}
